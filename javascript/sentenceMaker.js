775e5623393a679b13a5f9a117febf95a49f23c8
soasrez <soasres@outlook.com>
2025-01-28 15:19:54 -0300

    Create sentenceMaker.js

---
+++ javascript/sentenceMaker.js
@@ -0,0 +1,22 @@
+let adjective = "kind";
+let noun = "bird";
+let verb = "held";
+let place = "house"; 
+let adjective2 = "short";
+let noun2 = "noodle";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 +  " nostrils that blew fire when it was " + verb +".";
+
+console.log("First story: " + firstStory);
+
+adjective = "graceful";
+noun = "fox";
+verb = "jumped";
+place = "forest";
+adjective2 = "bright";
+noun2 = "lantern";
+
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 +  " nostrils that blew fire when it was " + verb +".";
+
+console.log("Second story: " + secondStory);
