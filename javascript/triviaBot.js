bc3cf9c71bafccd39b7a6be50bff1fc1cba759f9
soasrez <soasres@outlook.com>
2025-01-28 11:49:28 -0300

    Create triviaBot.js

---
+++ javascript/triviaBot.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName = "bot";
+let botLocation = "botland";
+let favoriteLanguage = "botlanguage";
+
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Fun fact! To this day I still got a lot to learn about " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "Another fun fact, you will always have something to learn about " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "And one more thing, there is a lot of potential in " + favoriteLanguage + ", so don't give up and keep going. You got this!"
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
